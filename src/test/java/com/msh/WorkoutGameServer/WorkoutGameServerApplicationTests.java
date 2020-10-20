09a0c91b9ee86f1292de824c11ea9381d4582b9c
VargaMV <48641905+VargaMV@users.noreply.github.com>
2020-10-20 18:23:12 +0200

    Initial commit

---
+++ src/test/java/com/msh/WorkoutGameServer/WorkoutGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msh.WorkoutGameServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
